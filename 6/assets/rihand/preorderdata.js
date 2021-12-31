68ada34f0be3a30fcf362bc603815e1783ced23f
Rihand Creative <tech.rihandcreative@gmail.com>
2021-12-31 17:44:47 +0700

    ORC-VI (Update 21-12-31)

--- 6/assets/rihand/preorderdata.js
+++ 6/assets/rihand/preorderdata.js
@@ -2,9 +2,4 @@ let userList = [
 	{ userid: "elfradodee779743", nama: "Elfrado Dee Vernandacello", sekolah: "SMPK SANTA MARIA II MALANG", bidang: "SMP IPS", peringkat: 2, medali: "Perak" },
 	{ userid: "chelistedee824813", nama: "Cheliste Dee Valeria", sekolah: "SMAK SANTA MARIA MALANG", bidang: "SMA Bahasa Inggris", peringkat: 5, medali: "Perunggu" },
 	{ userid: "josh098765", nama: "JOSH", sekolah: "SDN 123 LONDON", bidang: "SD IPS", peringkat: 1, medali: "Emas" },
-	{ userid: "ghozyaushaf657126", nama: "GHOZY AUSHAF HIBRIZI", sekolah: "SD ASY SYAFI'IYAH MEDAN JOHOR", bidang: "SD Bahasa Inggris", peringkat: 41, medali: "Perak" },
-	{ userid: "ghozyaushaf917356", nama: "GHOZY AUSHAF HIBRIZI", sekolah: "SD ASY SYAFI'IYAH MEDAN JOHOR", bidang: "SD Matematika", peringkat: 28, medali: "Peserta" },
-	{ userid: "mburhanuddin772676", nama: "M.BURHANUDDIN", sekolah: "SMA TA'MIRIYAH SURABAYA", bidang: "SMA Bahasa Inggris", peringkat: 20, medali: "Perak" },
-	{ userid: "mburhanuddin286970", nama: "M.BURHANUDDIN", sekolah: "SMA TA'MIRIYAH SURABAYA", bidang: "SMA IPA", peringkat: 3, medali: "Perak" },
-	{ userid: "mburhanuddin659118", nama: "M.BURHANUDDIN", sekolah: "SMA TA'MIRIYAH SURABAYA", bidang: "SMA Matematika", peringkat: 6, medali: "Perunggu" },
 ];
