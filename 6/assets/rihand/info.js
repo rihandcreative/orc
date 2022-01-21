bd47ce61a5cb364d05b2e5e53a3741196b4fdd4b
Rihand Creative <91003498+rihandcreative@users.noreply.github.com>
2022-01-21 18:04:17 +0700

    Added Cek Data Peserta
    - Added Cek Data Peserta
    - Added DataTables Plugin

---
+++ 6/assets/rihand/info.js
@@ -0,0 +1,7 @@
+$(document).ready( function () {
+    $('.table-data').DataTable({
+        "language": {
+            "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Indonesian.json"
+        }
+    });
+} );
