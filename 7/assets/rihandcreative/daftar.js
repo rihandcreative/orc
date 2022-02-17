46f3dd04c74b5233de9874a27ebcdb30511dcb58
rihandcreative <91003498+rihandcreative@users.noreply.github.com>
2022-02-17 11:54:08 +0700

    ORC-VII: Kisi-kisi

--- 7/assets/rihandcreative/daftar.js
+++ 7/assets/rihandcreative/daftar.js
@@ -75,7 +75,7 @@ if(getParams.submit !== undefined) {
         cancelButtonText: "Saya sudah join group dan ingin mengisi form lagi",
     }).then((result) => {
         if (result.isConfirmed) {
-            window.location = "https://rihand.link/orc7-group2";
+            window.location = "https://rihand.link/orc7-group3";
         } else {
             window.location = "./daftar";
         }
